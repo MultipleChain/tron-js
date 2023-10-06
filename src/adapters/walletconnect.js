3ebe9987129fc1f12a189f13e68ac544e55fc182
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2023-10-06 22:04:42 +0800

    removed unsupported wallets for walletconnect

--- src/adapters/walletconnect.js
+++ src/adapters/walletconnect.js
@@ -15,13 +15,9 @@ module.exports = walletconnect = (provider) => {
                 '--w3m-z-index': 999999999999,
             },
             themeMode: provider.wcThemeMode,
+            explorerExcludedWalletIds: "ALL",
             explorerRecommendedWalletIds: [
-                '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0',
-                '38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662',
-                '0b415a746fb9ee99cce155c2ceca0c6f6061b1dbca2d722b3ba16381d0562150',
-                '20459438007b75f4f4acb98bf29aa3b800550309646d375da5fd4aac6c2a2c66',
-                '971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709',
-                '7674bb4e353bf52886768a3ddc2a4562ce2f4191c80831291218ebd90f5f5e26'
+                '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0'
             ],
         },
     });
@@ -44,7 +40,7 @@ module.exports = walletconnect = (provider) => {
 
     return {
         key: 'walletconnect',
-        name: 'WalletConnect',
+        name: 'WalletConnect (Only Trust Wallet)',
         supports: [
             'browser',
             'mobile'
--- src/utils.js
+++ src/utils.js
@@ -11,7 +11,10 @@ module.exports = Object.assign(utils, {
             if (
                 String(error.message).includes('Confirmation declined by user') ||
                 String(error.message).includes('User rejected the request.') ||
-                String(error.message).includes('The user rejected connection.')
+                String(error.message).includes('The user rejected connection.') || 
+                String(error.message).includes('Modal is closed.') ||
+                String(error.message).includes('User canceled') ||
+                String(error.message).includes('User rejected') 
             ) {
                 return reject('request-rejected');
             } else if (String(error.message).includes('The wallet is not found.')) {
